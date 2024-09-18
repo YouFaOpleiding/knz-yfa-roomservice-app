12a7dbdddf0cb2c7d554afe146866326f6d831da
Mackenzie Schoon <m.schoon@yourfutureacademy.nl>
2024-09-18 15:04:32 +0200

    Added faSquarePlus icon

--- src/main.js
+++ src/main.js
@@ -4,14 +4,12 @@ import router from './router'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faCartPlus, faShoppingCart, faSquareMinus } from '@fortawesome/free-solid-svg-icons'
+import { faCartPlus, faShoppingCart, faSquareMinus, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
 
 import Curr from '@/components/Curr'
 import Product from '@/components/Product'
 
-library.add(faCartPlus)
-library.add(faShoppingCart)
-library.add(faSquareMinus)
+library.add(faCartPlus, faShoppingCart, faSquareMinus, faSquarePlus)
 
 createApp(App)
   .use(router)
