f7b070a7d35f25f6f57f27965db552b21d1186c4
Mackenzie Schoon <m.schoon@yourfutureacademy.nl>
2024-09-18 11:08:48 +0200

    Changed EOL to LF

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,19 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser'
   },
   rules: {
-    "vue/multi-word-component-names": "off",
-    "vue/no-multiple-template-root": "off",
-    "no-multi-spaces": ["error"],
-    "no-unused-vars": "off",
-    "no-use-before-define": "off",
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
-};
+    'vue/multi-word-component-names': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'no-multi-spaces': ['error'],
+    'no-unused-vars': 'off',
+    'no-use-before-define': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
